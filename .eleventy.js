a7247d2d2a23170802d63c5b74f065c367e78e09
Saurabh Joshi <sbjoshi@gmail.com>
2026-03-31 16:25:55 +0530

    feat(publications): Integrate YAML data parsing and remove old BibTeX parser

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,17 @@
  * @fileoverview Eleventy configuration file.
  */
 
+import yaml from 'js-yaml';
+
 /**
  * @param {import("@11ty/eleventy").UserConfig} eleventyConfig
  * @returns {import("@11ty/eleventy").UserConfig}
  */
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets');
+
+  // Add YAML support for data files
+  eleventyConfig.addDataExtension('yaml', (contents) => yaml.load(contents));
 
   // Create a collection for blog posts
   eleventyConfig.addCollection('posts', function(collectionApi) {
